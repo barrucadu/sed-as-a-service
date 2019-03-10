f9c0ccb4d5d9e412cb62387358d34a1d8e5d9cb6
Michael Walker <mike@barrucadu.co.uk>
2019-03-10 18:05:56 +0000

    Retrieve builder factory on POST request

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
@@ -26,17 +27,25 @@ public class Main {
         @Override
         public void handle(HttpExchange t) throws IOException {
             String method = t.getRequestMethod().toLowerCase();
-            String uri = t.getRequestURI().getPath().toLowerCase();
+            String unixUtility = t.getRequestURI().getPath().toLowerCase().substring(1);
             OutputStream os = t.getResponseBody();
 
-            System.out.println("method: '" + method + "', uri: '" + uri + "'");
+            System.out.println("method: '" + method + "', unix utility: '" + unixUtility + "'");
 
             int code = 400;
             String response = "Bad request";
 
+            UnixUtilityBuilderFactoryRegistrySingleton registry = UnixUtilityBuilderFactoryRegistrySingleton.getInstance();
+
             if (method.equals("post")) {
-                code = 404;
-                response = "Not found";
+                Optional<UnixUtilityBuilderFactory> builderFactory = registry.retrieve(unixUtility);
+                if (builderFactory.isPresent()) {
+                    code = 501;
+                    response = "Not implemented";
+                } else {
+                    code = 404;
+                    response = "Not found";
+                }
             } else if (method.equals("put")) {
                 code = 404;
                 response = "Not found";
