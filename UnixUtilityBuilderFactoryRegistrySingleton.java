debcdac47f3d2f257be7668cf59c286ebb0ce12c
Michael Walker <mike@barrucadu.co.uk>
2019-03-10 18:00:08 +0000

    Store UnixUtilityBuilderFactories in the registry

--- UnixUtilityBuilderFactoryRegistrySingleton.java
+++ UnixUtilityBuilderFactoryRegistrySingleton.java
@@ -1,5 +1,9 @@
+import java.util.HashMap;
+
 public class UnixUtilityBuilderFactoryRegistrySingleton {
     private static final UnixUtilityBuilderFactoryRegistrySingleton instance = new UnixUtilityBuilderFactoryRegistrySingleton();
+
+    private HashMap<String, UnixUtilityBuilderFactory> registry = new HashMap();
 
     private UnixUtilityBuilderFactoryRegistrySingleton() {}
 
@@ -7,6 +11,7 @@ public class UnixUtilityBuilderFactoryRegistrySingleton {
         return instance;
     }
 
-    public void register(String unixUtility, UnixUtilityBuilderFactory factory) {
+    public synchronized void register(String unixUtility, UnixUtilityBuilderFactory factory) {
+        registry.put(unixUtility, factory);
     }
 }
